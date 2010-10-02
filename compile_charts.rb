c9b9a2c34088c2a9345eaee0aa0ad5b6a9b61fe6
Adam Thorsen <adam.thorsen@gmail.com>
2010-10-01 20:47:10 -0700

    Properly handle lack of chart arg

--- compile_charts.rb
+++ compile_charts.rb
@@ -24,7 +24,7 @@ OptionParser.new do |opts|
     options[:no_data] = true 
   end
 
-  opts.on("-c", "--chart CHART") do |c|
+  opts.on("-c", "--chart [CHART]") do |c|
     options[:chart] = c
   end
 
@@ -33,28 +33,31 @@ end.parse!
 formatters_path = File.join(File.dirname(__FILE__), 'formatters')
 chart_config = CashDash::Config.instance.get_config['charts']
 charts = options[:chart].nil? ? chart_config : {options[:chart] => chart_config[options[:chart]]}
-if(!chart_config[options[:chart]].nil?)
-  charts.each_pair do |name, config|
-    require File.join(formatters_path, name)
-    chart = Chart.get(name) 
-    if(!options[:no_data])
-      config['series'] = Kernel.const_get(camelize(name)).generate_series
-    end
-    if chart.nil?
-      Chart.create(:name => name, :config => config.to_json)
+
+#Make sure we have a config for the chart
+if(options[:chart] && chart_config[options[:chart]].nil?)
+  puts "Unkown chart: #{options[:chart]}"
+  exit(0)
+end
+
+charts.each_pair do |name, config|
+  require File.join(formatters_path, name)
+  chart = Chart.get(name) 
+  if(!options[:no_data])
+    config['series'] = Kernel.const_get(camelize(name)).generate_series
+  end
+  if chart.nil?
+    Chart.create(:name => name, :config => config.to_json)
+  else
+    if config['series']
+      chart.config = config.to_json
+      chart.save
     else
-      if config['series']
-        chart.config = config.to_json
-        chart.save
-      else
-        config['series'] = JSON.parse(chart.config)['series']
-        chart.config = config.to_json
-        chart.save
-      end
+      config['series'] = JSON.parse(chart.config)['series']
+      chart.config = config.to_json
+      chart.save
     end
   end
-else
-  puts "Unkown chart: #{options[:chart]}"
 end
 
 
