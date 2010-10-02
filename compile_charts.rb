203c6e56cc90a91ec16e5019c74ba422110b4694
Adam Thorsen <adam.thorsen@gmail.com>
2010-10-01 20:26:26 -0700

    Options to allow building individual charts and tweaking chart settings without refreshing the data

--- compile_charts.rb
+++ compile_charts.rb
@@ -2,6 +2,7 @@
 require 'rubygems'
 require 'bundler/setup'
 
+require 'optparse'
 require 'json'
 require 'yaml'
 require 'mixpanel_client'
@@ -14,17 +15,46 @@ def camelize(str)
   str.split('_').map {|w| w.capitalize}.join
 end
 
-formatters_path = File.join(File.dirname(__FILE__), 'formatters')
-CashDash::Config.instance.get_config['charts'].each_pair do |name, config|
-  require File.join(formatters_path, name)
-  config['series'] = Kernel.const_get(camelize(name)).generate_series
-  chart = Chart.get(name) 
-  if chart.nil?
-    Chart.create(:name => name, :config => config.to_json)
-  else
-    chart.config = config.to_json
-    chart.save
+options = {}
+
+OptionParser.new do |opts|
+  opts.banner = "Usage: compile_charts.rb [options]"
+
+  opts.on("-n", "--no-data", "Skip data gathering") do |n|
+    options[:no_data] = true 
   end
+
+  opts.on("-c", "--chart CHART") do |c|
+    options[:chart] = c
+  end
+
+end.parse!
+
+formatters_path = File.join(File.dirname(__FILE__), 'formatters')
+chart_config = CashDash::Config.instance.get_config['charts']
+charts = options[:chart].nil? ? chart_config : {options[:chart] => chart_config[options[:chart]]}
+if(!chart_config[options[:chart]].nil?)
+  charts.each_pair do |name, config|
+    require File.join(formatters_path, name)
+    chart = Chart.get(name) 
+    if(!options[:no_data])
+      config['series'] = Kernel.const_get(camelize(name)).generate_series
+    end
+    if chart.nil?
+      Chart.create(:name => name, :config => config.to_json)
+    else
+      if config['series']
+        chart.config = config.to_json
+        chart.save
+      else
+        config['series'] = JSON.parse(chart.config)['series']
+        chart.config = config.to_json
+        chart.save
+      end
+    end
+  end
+else
+  puts "Unkown chart: #{options[:chart]}"
 end
 
 
